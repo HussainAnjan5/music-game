42306edabccf3a2a1984e5e8227470b6fd0d61b0
Muhammad Hussain <muhammad.hussain.cs8@gmail.com>
2024-10-02 12:27:05 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,28 +50,26 @@ function addVariablesForColors({ addBase, theme }: PluginUtils) {
 }
 
 // Plugin to add SVG patterns for backgrounds
-function addSvgPatterns({ matchUtilities, theme }: MatchUtilities) {
-  const backgroundColors = flattenColorPalette(theme('backgroundColor')) as Record<string, string>;
-
+function addSvgPatterns({ matchUtilities, theme }: any) {
   matchUtilities(
     {
-      'bg-grid': (value: string) => ({
+      'bg-grid': (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
         )}")`,
       }),
-      'bg-grid-small': (value: string) => ({
+      'bg-grid-small': (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
         )}")`,
       }),
-      'bg-dot': (value: string) => ({
+      'bg-dot': (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
-          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
         )}")`,
       }),
     },
-    { values: backgroundColors, type: 'color' }
+    { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
   );
 }
 
